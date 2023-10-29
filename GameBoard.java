eeb2799ce1bdbf4c2b9caa65fd7ae20c29d8cb30
aaron "hyperion <75545851+hyperionao@users.noreply.github.com>
2023-10-28 22:24:54 -0400

    Added ability to restart, new session, win and lose conditions, and revealing mines

--- GameBoard.java
+++ GameBoard.java
@@ -24,33 +24,110 @@ public class GameBoard extends JPanel{
         }
     }
 
-    public GameBoard(Cell[][] cells) {
+    JButton restartButton = new JButton("Restart");
+    restartButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+        // Reset the game board and start a new game
+        resetGame();
+        }
+    });
+
+    public GameBoard(Cell[][] cells) 
+    {
         this.cells = cells;
 
         loadCellImages(); // load to cell images from files
 
-        addMouseListener(new MouseAdapter() {
+        addMouseListener(new MouseAdapter() 
+        {
             @Override
-            public void mouseClicked(MouseEvent e){
+            public void mouseClicked(MouseEvent e)
+            {
                 int x = e.getX() / Cell.CELL_SIZE;
                 int y = e.getY() / Cell.CELL_SIZE;
                 Cell cell = cells[y][x];
 
-                if (SwingUtilities.isLeftMouseButton(e)){
-                    if (!cell.isFlagged()){
-                        // handle left click to reveal cells
-                        cell.reveal();
-                        repaint(); // redraw board
-                    }
-                } else if (SwingUtilities.isRightMouseButton(e))
+                if (SwingUtilities.isLeftMouseButton(e))
                 {
+                    if (!cell.isFlagged())
+                    {
+
+                        int col = e.getX() / Cell.CELL_SIZE;
+                        int row = e.getY() / Cell.CELL_SIZE;
+
+                        if (col >= 0 && col < cells[0].length && row >= 0 && row < cells.length) 
+                        {
+                            Cell clickedCell = cells[row][col];
+
+                            if (clickedCell.isMine())
+                            {
+                                // Handle mine click (game over here)
+                                // end game, reveal all mines, and show a game over
+                                for (int row = 0; row < cells.length; row++) {
+                                    for (int col = 0; col < cells[0].length; col++) {
+                                        Cell cell = cells[row][col];
+                                        if (cell.isMine()) {
+                                            cell.reveal();
+                                        }
+                                    }
+                                }
+                                repaint(); 
+                            } else 
+                            {
+                                clickedCell.reveal();
+                                repaint(); // redraw board
+
+                                if (clickedCell.isEmpty())
+                                {
+                                    revealAdjacentEmptyCells(cells, row, col);
+                                }
+
+                                if (checkWinCondition())
+                                {
+                                    //Handle win condition here (show a win screen me thinks)
+                                }
+                            }
+                        // handle left click to reveal cells
+                        }
+                    }
+                } else if (SwingUtilities.isRightMouseButton(e)) {
                     //Handle right-click on cell (flag/unflag)
                     cell.toggleFlag();
                     repaint();
                 }
-            }            
+            }
         });
     }
+    
+    private void revealAdjacentEmptyCells(Cell[][] cells, int row, int col) {
+        // Define the possible adjacent directions
+        int[][] directions = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1},           {0, 1},
+            {1, -1}, {1, 0}, {1, 1}
+        };
+    
+        for (int[] dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+    
+            // Check if the new coordinates are within the grid
+            if (newRow >= 0 && newRow < cells.length && newCol >= 0 && newCol < cells[0].length) {
+                Cell adjacentCell = cells[newRow][newCol];
+    
+                if (!adjacentCell.isRevealed() && !adjacentCell.isFlagged()) {
+                    adjacentCell.reveal();
+                    repaint();
+    
+                    if (adjacentCell.isEmpty()) {
+                        // Recursively reveal adjacent empty cells
+                        revealAdjacentEmptyCells(cells, newRow, newCol);
+                    }
+                }
+            }
+        }
+    }
+    
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -68,6 +145,35 @@ public class GameBoard extends JPanel{
             }
         }
     }
+    private boolean checkWinCondition() {
+        for (int row = 0; row < cells.length; row++) {
+            for (int col = 0; col < cells[0].length; col++) {
+                Cell cell = cells[row][col];
+                
+                if (!cell.isRevealed() && !cell.isMine()) {
+                    return false; // A non-revealed, non-mine cell found, game not won
+                }
+            }
+        }
+        
+        return true; // All non-mine cells are revealed, game won
+    }
+    private void resetGame() {
+        // Reset the game board by reinitializing the grid and game logic
+        // You may also need to reset any game-related variables or flags.
+        // Repaint the game board to display the initial state.
+    }
+    
+    private void startNewGame() {
+        // Start a new game with a new mine arrangement by creating a new instance of the game logic.
+        // Reinitialize the grid, clear any flags, and set up a new game.
+        // Repaint the game board to display the new game.
+    }
+
+    
 }
+
+
+
 
 // keep track of what we want Cell.java to be about, note CELL_SIZE, .getCellState, .toggleFlag, .isFlagged, .reveal(), 
